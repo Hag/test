d3e46c71aa336b0d2c973058d185f21e42f06a05
Hag <n.haghn@gmail.com>
2009-11-28 02:12:27 +0100

    minor update

--- TEngine.hpp
+++ TEngine.hpp
@@ -1,23 +1,41 @@
 #ifndef TENGINE_H
 #define TENGINE_H
 
+#include <queue>
+
+
 class CGame;
+class CMessage;
+class GEngine;
+class MEngine;
+
+typedef std::queue<CMessage> CMQ;
+
 
 class TEngine
 {
 public :
+
   TEngine(CGame* Parent);
   virtual ~TEngine()=0;
 
   virtual void frame()=0;
 
+  void push(CMessage& m);
+  void processCMs();
+  virtual void processCM(CMessage* m;);
+
   void linkGE(GEngine* GE);
-  void linkME(GEngine* ME);
+  void linkME(MEngine* ME);
 
 private :
+
   CGame* _Parent;
   GEngine* _Ge;
   MEngine* _Me;
+
+  CMQ* _CMqueue;
+
 };
 
 #endif
